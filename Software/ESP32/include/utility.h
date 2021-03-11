ebc896f0643f8aa0e3fcfe260ab96a418ef01ca9
MRo47 <rodriguesmyron47@gmail.com>
2021-03-11 09:47:21 +0000

    added constants

--- Software/ESP32/include/utility.h
+++ Software/ESP32/include/utility.h
@@ -12,7 +12,9 @@ struct PhData
 namespace constants
 {
     static const float adc_res = 1024.0;
+    static const float adc_ref_v = 3.3;
     static const float ph7_volt_at_25C = 1134;
+    static const long ph_calib_timeout = 30000; //30 sec
 }
 
 #endif
