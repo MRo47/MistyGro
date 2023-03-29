7694286bb6217cb98b8f8e2bed6a3b7d662436ff
MRo47 <rodriguesmyron47@gmail.com>
2023-03-29 23:37:32 +0100

    top level interfacing started

--- Software/ESP32/include/utility.h
+++ Software/ESP32/include/utility.h
@@ -19,6 +19,7 @@ static const long ec_calib_timeout = 20000;  // 20 sec
 static const int ec_samples = 10;            // samples taken for computing mean
 static const int ec_calib_magic = 99;        // if ec calib found
 static const int ph_calib_magic = 88;        // if calib found
+static const int ph_samples = 10;            // samples of ph to compute mean
 static const PhCalib ph_low_limit{1830, 1300, 775};    // ph low limits
 static const PhCalib ph_high_limit{2230, 1700, 1175};  // ph high limits
 static const PhCalib default_ph_data{2030, 1500, 975};
@@ -35,6 +36,10 @@ static const int temp_sensor_bus = 32;  // pin
 static const int adc_scl = 22;
 static const int adc_sda = 21;
 static const int ec_enable = 17;
+static const int misters = 12;
+static const int light = 14;
+static const int extra_relay = 27;
+static const int ph_enable = 5;
 }  // namespace pin
 
 #endif
--- Software/ESP32/src/main.cpp
+++ Software/ESP32/src/main.cpp
@@ -1,22 +1,27 @@
 #include <Arduino.h>
 
 #include "custom_eeprom.h"
+#include "ec_meter.h"
 #include "ph_sensor.h"
 #include "relay.h"
+#include "utility.h"
 
-RelayAL misters(12);
-RelayAL light(14);
-RelayAH extra_out(27);
-PHSensor ph_sensor(1, 2, 3);
+ADC adc;
+RelayAL misters(pin::misters);
+RelayAL light(pin::light);
+RelayAH extra_relay(pin::extra_relay);
+ECMeter ec_meter(pin::ph_enable, constants::ec_samples, &adc);
+PHSensor ph_sensor(pin::ph_enable, constants::ph_samples, &adc);
 CustomEEPROM & eeprom(CustomEEPROM::get_instance());
 
 void setup()
 {
   Serial.begin(115200);
   eeprom.begin();
+  adc.begin(constants::adc_bus_addr, pin::adc_sda, pin::adc_scl);
   misters.begin(Switch::ON);
   light.begin(Switch::ON);
-  extra_out.begin(Switch::OFF);
+  extra_relay.begin(Switch::OFF);
   ph_sensor.begin();
   delay(1000);
 }
