54416555c67fab97e21383ab28260f7414698590
MRo47 <rodriguesmyron47@gmail.com>
2023-06-03 11:36:02 +0100

    add timer

--- Software/ESP32/include/scheduler.h
+++ Software/ESP32/include/scheduler.h
@@ -1,25 +1,31 @@
 #ifndef SCHEDULER_H_
 #define SCHEDULER_H_
 
+#include <time.h>
+
+#include "timer.h"
+
 class Scheduler
 {
 public:
   Scheduler();
 
-  void create_task(void (*task_func)(), unsigned long interval);
+  void create_task(void (*task_func)(), unsigned long interval_sec);
   void run();
+  void begin(long int timer_sync_interval_ms = 600000);  // 10 mins
 
 private:
   struct TaskInfo
   {
     void (*task_func)();
     unsigned long interval;
-    unsigned long last_execution_time;
+    time_t last_execution_time;
   };
 
   static const int MAX_TASKS = 10;  // Maximum number of tasks
   TaskInfo tasks_[MAX_TASKS];
   int num_tasks_;
+  Timer timer_;
 };
 
 #endif  // SCHEDULER_H_
--- Software/ESP32/src/scheduler.cpp
+++ Software/ESP32/src/scheduler.cpp
@@ -4,11 +4,11 @@
 
 Scheduler::Scheduler() : num_tasks_(0) {}
 
-void Scheduler::create_task(void (*task_func)(), unsigned long interval)
+void Scheduler::create_task(void (*task_func)(), unsigned long interval_sec)
 {
   if (num_tasks_ < MAX_TASKS) {
     tasks_[num_tasks_].task_func = task_func;
-    tasks_[num_tasks_].interval = interval;
+    tasks_[num_tasks_].interval = interval_sec;
     tasks_[num_tasks_].last_execution_time = 0;
     num_tasks_++;
   }
@@ -17,14 +17,18 @@ void Scheduler::create_task(void (*task_func)(), unsigned long interval)
 void Scheduler::run()
 {
   for (;;) {
-    unsigned long current_millis = millis();
+    time_t current_time = timer_.get_epoch_time();
     for (int i = 0; i < num_tasks_; i++) {
-      if (
-        current_millis - tasks_[i].last_execution_time >= tasks_[i].interval) {
-        tasks_[i].last_execution_time = current_millis;
+      if (current_time - tasks_[i].last_execution_time >= tasks_[i].interval) {
+        tasks_[i].last_execution_time = current_time;
         tasks_[i].task_func();
       }
     }
     // Additional code to be executed in the main loop can go here
   }
+}
+
+void Scheduler::begin(long int timer_sync_interval_ms)
+{
+  timer_.begin(timer_sync_interval_ms);
 }
