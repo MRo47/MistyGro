4641d0bf1f459f983450eb2e25a61a141b68bf56
MRo47 <rodriguesmyron47@gmail.com>
2023-06-03 11:36:29 +0100

    tested scheduler

--- Software/ESP32/src/test_scheduler.cpp
+++ Software/ESP32/src/test_scheduler.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 #include "scheduler.h"
+#include "secrets.h"
 
 Scheduler scheduler;
 
@@ -11,8 +12,10 @@ void task2() { Serial.println("Task 2"); }
 void setup()
 {
   Serial.begin(115200);
-  scheduler.create_task(task1, 2000);
-  scheduler.create_task(task2, 5000);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  scheduler.begin(10000);
+  scheduler.create_task(task1, 2);
+  scheduler.create_task(task2, 5);
 }
 
 void loop() { scheduler.run(); }
