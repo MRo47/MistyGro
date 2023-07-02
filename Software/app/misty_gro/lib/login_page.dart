57c17d9c8f848fdc5114720b69802048a663346d
MRo47 <rodriguesmyron47@gmail.com>
2023-07-02 17:46:33 +0100

    added login page

--- Software/app/misty_gro/lib/login_page.dart
+++ Software/app/misty_gro/lib/login_page.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -28,6 +28,76 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('hello'));
+    return Scaffold(
+      // Sets the content to the
+      // center of the application page
+      body: Container(
+        margin: const EdgeInsets.only(left: 20, right: 20),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Image(
+              image: AssetImage('assets/mistygro_512.png'),
+              width: 150,
+            ),
+            const Text(
+              'MistyGro',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                  filled: true,
+                  hintText: "Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              controller: _passController,
+              obscureText: true,
+              decoration: InputDecoration(
+                  filled: true,
+                  hintText: "Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Sign in',
+                  style: TextStyle(fontSize: 27, fontWeight: FontWeight.w700),
+                ),
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.green.shade600,
+                  child: IconButton(
+                      color: Colors.white,
+                      onPressed: () {
+                        signIn();
+                      },
+                      icon: const Icon(
+                        Icons.arrow_forward,
+                      )),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
