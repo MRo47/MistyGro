7202342f81547db7276858c9bafa7631adeb3760
MRo47 <rodriguesmyron47@gmail.com>
2023-09-22 12:46:07 +0100

    scheduler api docs

--- Software/ESP32/include/scheduler.h
+++ Software/ESP32/include/scheduler.h
@@ -1,3 +1,5 @@
+// Partly created with chat GPT 3.5
+
 #ifndef SCHEDULER_H_
 #define SCHEDULER_H_
 
@@ -5,16 +7,45 @@
 
 #include "timer.h"
 
+/**
+ * @brief A scheduler for running tasks on the controller, minimum time between events is 1 second
+ * based off the ntc timer. Also depends how slow is the loop function. 
+ * 
+ * Maximum 10 tasks can be added, increase in the header file (MAX_TASKS) if required 
+ * but beware of exhausting the heap
+ */
 class Scheduler
 {
 public:
   Scheduler();
 
+  /**
+   * @brief Create a task
+   * 
+   * @param task_func a task function that takes no input and has void output
+   * @param interval_sec 
+   */
   void create_task(void (*task_func)(), unsigned long interval_sec);
+
+  /**
+   * @brief This needs to be run in the loop function continuously. Best practice is
+   * to have a loop function that only runs the run() method of this scheduler.
+   */
   void run();
+
+  /**
+   * @brief initialise timer
+   * 
+   * @param timer_sync_interval_ms time interval in milliseconds to sync the internal ntc clock
+   */
   void begin(long int timer_sync_interval_ms = 600000);  // 10 mins
 
 private:
+
+  /**
+   * @brief Task info which stores info on a task to be run
+   * 
+   */
   struct TaskInfo
   {
     void (*task_func)();
