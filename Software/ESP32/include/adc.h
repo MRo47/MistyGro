a13d7780799e106acbf21850134f8f3b6011b9a7
MRo47 <rodriguesmyron47@gmail.com>
2023-09-22 12:16:24 +0100

    adc api docs

--- Software/ESP32/include/adc.h
+++ Software/ESP32/include/adc.h
@@ -17,10 +17,29 @@ private:
 public:
   explicit ADC();
 
+  /** initialise i2c, ADS1115 and set gain to 2/3
+   * @brief 
+   * 
+   * @param addr ADS i2c address
+   * @param sda_pin 
+   * @param scl_pin 
+   */
   void begin(int addr, int sda_pin, int scl_pin);
 
+  /**
+   * @brief Read ADC value from channel
+   * 
+   * @param ch ADC channel of ADS1115
+   * @return int16_t : adc value
+   */
   int16_t read(ADCChannel ch);
 
+  /**
+   * @brief Read voltage from ADC channel (computes voltage using the set fixed gain)
+   * 
+   * @param ch ADC channel
+   * @return float : voltage
+   */
   float read_voltage(ADCChannel ch);
 };
 
