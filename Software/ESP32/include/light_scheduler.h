fc421889b8a22a65687e1308386db71699698f27
MRo47 <rodriguesmyron47@gmail.com>
2023-09-22 12:27:14 +0100

    light scheduler api docs

--- Software/ESP32/include/light_scheduler.h
+++ Software/ESP32/include/light_scheduler.h
@@ -5,6 +5,10 @@
 
 #include "relay.h"
 
+/**
+ * @brief Schedules light switching depending on time of day and ambient light
+ * 
+ */
 class LightScheduler
 {
 private:
@@ -16,11 +20,30 @@ private:
   int start_min_;
 
 public:
+  /**
+   * @brief Construct a new Light Scheduler object
+   * 
+   * @param light Relay used for light
+   * @param start_hour start hour in 24h format
+   * @param start_min start min 
+   * @param duration_sec total light duration required for the plant in seconds
+   */
   LightScheduler(
     Relay * light, int start_hour, int start_min, uint64_t duration_sec);
 
+  /**
+   * @brief reset start time to given time
+   * 
+   * @param time time vale to set to
+   */
   void reset(tm * time);
 
+  /**
+   * @brief Run this in a loop and pass the current time and a boolean indicating if its bright
+   * 
+   * @param time time struct of time now
+   * @param is_bright boolean flag if its bright now
+   */
   void run(tm * time, bool is_bright);
 };
 
